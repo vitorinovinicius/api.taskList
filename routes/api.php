195ec78f0945a99a7f7d56e481a0a92bba955f05
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-06-23 16:02:36 -0300

    Inserção de comentários pertinente ao projeto.

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Esta rota lista todas as tarefas.
 Route::get('/tasks', [TaskController::class, 'index']);
 
+//Esta rota lista somente uma única tarefa.
 Route::get('/task/{id}', [TaskController::class, 'show']);
 
+//Esta rota insere uma nova tarefa.
 Route::post('/task', [TaskController::class, 'store']);
 
+//Esta rota edita uma única tarefa.
 Route::put('/task/{id}', [TaskController::class, 'update']);
 
+//Esta rota deleta uma tarefa.
 Route::delete('/task/{id}', [TaskController::class, 'destroy']);
